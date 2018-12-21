65ce580068d3c508867ead786850fc90e62099f9
ilovezy <1518265554@qq.com>
2018-12-21 10:51:50 +0800

    update

---
+++ pages/search/searchBook/searchBook.vue
@@ -0,0 +1,64 @@
+<template>
+	<view>
+        <van-search :value="value" placeholder="请输入搜索关键词" show-action @search="onSearch"  @cancel="onCancel"></van-search>  
+           
+        <view class="search-tag-wrap">
+            <view class="title title-with-icon">
+                <view>大家都在搜 </view>
+                <view class="iconfont icon-yaoqingjiangli" @click="doShuffle"></view>
+            </view>
+            
+            <view class="tag-wrap tag-all">
+                <view v-for="(item, index) in tagList" :key="index" class="tag-item " :class="randomType[index]">
+                    {{item}}
+                </view>
+            </view>
+        </view>
+        <view class="place-bg"></view>
+
+        <view class="search-tag-wrap ">
+            <view class="title title-with-icon">
+                <view>搜索历史</view>
+                <view class="iconfont icon-yaoqingjiangli"></view>
+            </view>
+            
+            <view class="tag-wrap tag-history">
+            	<view v-for="(item, index) in tagHistoryList" :key="index" class="tag-item">
+            		{{item}}
+            	</view>
+            </view>
+        </view>
+    
+	</view>
+</template>
+
+<script>
+  import _ from '@/common/underscore'
+export default {
+    data() {
+        return {
+            value: '',
+            tagList: ['热死', 'toke', 'very hot', 'haha', '反倒是建安费', '黑哦', 'haha反倒', 'fdah检查vfoivv'],
+            tagHistoryList: ['方面的哦啊', '资产', '农夫', '没发过', '呵呵呵', 'fdaj'],
+            randomType:  _.shuffle(['tag-1','tag-2','tag-3','tag-4','tag-5','tag-6','tag-7','tag-8','tag-9','tag-10'])
+        };
+    },
+
+    methods: {
+      doShuffle(){
+        this.tagList = _.shuffle(this.tagList)
+        this.randomType = _.shuffle(this.randomType)
+      },
+        onSearch() {
+            plus.nativeUI.alert('fuck');
+        },
+        onCancel() {
+            plus.nativeUI.alert('cancel');
+        }
+    }
+};
+</script>
+
+<style lang="less">
+@import 'searchBook.less';
+</style>
---
+++ pages/search/searchResult/searchResult.vue
@@ -0,0 +1,104 @@
+<template>
+	<view class="search-result-page">
+		<view v-for="(item, index) in list" :key="index" class="search-list-item">
+            <image class="cover" :src="item.cover" mode="scaleToFill"></image>
+            
+            <view class="detail-container">
+                <view class="top-container">
+                    <view class="header">
+                        <view class="header-left">
+                            <view class="title">
+                                {{item.title}}
+                            </view>
+                            <view class="writer">
+                                <view class="iconfont icon-wode"></view>
+                                <view class="writer-name">{{item.writer}}</view>
+                            </view>
+                        </view>
+                        <view class="header-right">
+                            <view class="add-book-shelf" @click="addToBookShelf">
+                                加书架
+                            </view>
+                        </view>
+                    </view>
+                    
+                    <view class="detail-desc">
+                        {{item.desc}}
+                    </view>
+                </view>
+                <view class="detail-footer">
+                	<view class="detail-tag" v-for="(item2, index2) in item.tags" :key="index2">
+                		{{item2}}
+                	</view>
+                </view>
+            </view>
+        </view>
+	</view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list: [
+                {
+                    cover: '../../../static/lolis/a.png',
+                    title: '心理罪城市之光',
+                    writer: '小蜜蜂',
+                    desc: '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法发动机搜啊方法',
+                    tags: ['完结', '27万字', '悬疑类', '现代27万字']
+                },
+                {
+                    cover: '../../../static/lolis/b.png',
+                    title: '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法',
+                    writer: '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法',
+                    desc: '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法',
+                    tags: ['完结', '27万字', '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法', '现代']
+                },
+                {
+                    cover: '../../../static/lolis/c.png',
+                    title: 'moifda发',
+                    writer: '小蜜蜂fdas',
+                    desc: '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法',
+                    tags: ['完结', '27万字', '悬疑类', '现代']
+                },
+                {
+                    cover: '../../../static/lolis/d.png',
+                    title: '非机动啊',
+                    writer: '小蜜蜂',
+                    desc: '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法',
+                    tags: ['完结', '悬疑类', '现代']
+                },
+                {
+                    cover: '../../../static/lolis/dd.jpg',
+                    title: '心理罪城之光',
+                    writer: '小蜜蜂',
+                    desc: '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法',
+                    tags: ['完结', '27万字', '悬疑类', '现代']
+                },
+                {
+                    cover: '../../../static/lolis/a.png',
+                    title: '心理光',
+                    writer: '小蜜蜂',
+                    desc: '喝酒诶哦发货到你丰富的睡觉奥if发的撒娇发动机搜啊方法',
+                    tags: ['完结']
+                }
+            ]
+        };
+    },
+
+    methods: {
+        goBookDetail() {
+            plus.nativeUI.alert('前往书本详情');
+        },
+
+        addToBookShelf() {
+            plus.nativeUI.alert('添加成功');
+        }
+    }
+};
+</script>
+
+<style lang="less">
+@import 'searchResult.less';
+</style>
